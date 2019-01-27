d14505b9cdb8167aa2cdf7ca8485d45a0f1b3e7c
z-gonzales <zoegonzales03@gmail.com>
2019-01-27 13:54:35 -0700

    reorganized js file for clarity, added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,31 @@
 
 $(document).ready(function(){
+    // GLOBAL VARIABLES
     // Array includes all of the intial buttons to be displayed
     var topics = ["Hey Arnold", "Fresh Prince", "Aaahh!!! Real Monsters", "Daria", "All That", "Courage the Cowardly Dog", "Pokémon", "Sailor Moon", "CatDog", "PowerPuff Girls"];
 
-    // Media Files
+    // Media Files    
     // Daria
     var daria = $("<audio>");
     daria.attr("src", "./assets/media/Daria.mp3");
-    $("#daria-play").on("click", function(){
-        daria.get(0).play();
-    });
-    
     // Hey Arnold
     var arnold = $("<audio>");
     arnold.attr("src", "./assets/media/HeyArnold.mp3");
-    $("#arnold-play").on("click", function(){
-        arnold.get(0).play();
-    });
-
     // Fresh Prince
     var prince = $("<audio>");
     prince.attr("src", "./assets/media/FreshPrince.mp3");
-    $("#prince-play").on("click", function(){
-        prince.get(0).play();
-    });
-
     // Sailor Moon
     var sailor = $("<audio>");
     sailor.attr("src", "./assets/media/SailorMoon.mp3");
-    $("#sailor-play").on("click", function(){
-        sailor.get(0).play();
-    });
-
     // PowerPuff Girls
     var power = $("<audio>");
     power.attr("src", "./assets/media/PowerpuffGirls.mp3");
-    $("#power-play").on("click", function(){
-        power.get(0).play();
-    });
-
     // All That
     var allThat = $("<audio>");
     allThat.attr("src", "./assets/media/AllThat.mp3");
-    $("#all-play").on("click", function(){
-        allThat.get(0).play();
-    });
-
-    // Pauses any of the buttons
-    $("#pause").on("click", function(){
-        daria.get(0).pause();
-        arnold.get(0).pause();
-        prince.get(0).pause();
-        sailor.get(0).pause();
-        power.get(0).pause();
-        allThat.get(0).pause();
-    });
-
+    
     // FUNCTIONS
-    // Function displays buttons dynamically - loops through the topics array
+    // Populates topic buttons
     function renderButtons() {
         // start by emptying the button-area div every time so that there are no duplicates
         $("#button-area").empty();
@@ -74,14 +42,11 @@ $(document).ready(function(){
             $("#button-area").append(button);
         } 
     }
-
-    // CLICK EVENTS
-    // When user clicks on one of the pre-populated buttons
-    $(document).on("click", ".tv-show", function(event){
+    // Populates gifs
+    function renderGifs() {
         // saves data of clicked button
         var show = $(this).data("name");
         var queryURL = `https://api.giphy.com/v1/gifs/search?q=${show}&limit=10&rating=pg-13&api_key=naqRbjAruZNru757XG6cSQyLUVmUQ3EC`;
-
         // AJAX call
         $.ajax({
             url: queryURL,
@@ -120,9 +85,9 @@ $(document).ready(function(){
                 $("#gif-area").prepend(newGif);
             }
         });
-    });
-
-    $(document).on("click", ".gif", function(){
+    }
+    // Allows toggling between animated and still states of each gif
+    function animateGifs() {
         var state = $(this).attr("data-state");
         var animated = $(this).attr("data-animate");
         var still = $(this).attr("data-still");
@@ -133,10 +98,9 @@ $(document).ready(function(){
             $(this).attr("src", still);
             $(this).attr("data-state", "still");
         }
-    });
-        
-    // When user inputs new topic into the #tv-input div and clicks #add-show
-    $("#add-show").on("click", function(event){
+    }
+    // Enables addition of button via input field
+    function addShow(event) {
         // prevent default method to avoid submit form default
         event.preventDefault();
         // Get value of the user's input and save to var
@@ -145,39 +109,89 @@ $(document).ready(function(){
         topics.push(input);
         // rerun function to populate buttons
         renderButtons();
-    });
-
-    $("#gif-area").on("click", ".add-fav", function(event){
-        // save the gif div to a variable
-        var addBtn = $(this)[0].firstChild;
-        addBtn.data = "Remove from favorites";
+    }
+    // Enables individual addition of targeted gif under Favorites section
+    function addFavorites() {
+        // changing text of button with add-fav class
+        var addBtn = $(this)[0];
+        addBtn.firstChild.data = "Added to favorites";
+        // creating a new div that will hold clone of favorite gif
         var newFav = $("<div>");
         newFav.addClass("new-fav");
+        // grabs info from selected div and clones it
         var favGif = $(this).prevAll().clone();
+        // creates button allowing for removal of favorited gif
         var removeFav = $("<button>");
-        removeFav.addClass("btn btn-info my-fav");
-        removeFav.attr("id", "remove");
-        removeFav.text(`Remove from favorites`);
+        removeFav.addClass("btn btn-info my-fav remove");
+        removeFav.text(`Remove from Favorites`);
+        // appending gif and removal button to newFav div
         newFav.append(favGif[1], removeFav);
+        // adding newFav div to favorites section
         $("#fav").append(newFav);
-
-        $(addBtn, removeFav).on("click", function(){
-        });
+        // running function to allow for removal of favorite gifs
+        removeFavs();
+        // Local Storage 
         // save variable in cookies so that the page refresh won't wipe it clean
-        // ^ include a "remove from favorites button for each div on this page"
         // sessionStorage.setItem("favorite", $("img"));
         // console.log(sessionStorage);
         // var favoritesArea = $("#favorites-area");
-    });
-
-    $("#remove").on("click", function(){
-        
-    });
-
-    // Click event that empties everything from the favs section
-    $("#clear-favs").on("click", function(){
+    }
+    // Enables individual removal of targeted gif under Favorites section
+    function removeFavs() {
+        if ($("button").hasClass("remove")) {
+            $(".remove").on("click", function(){
+                var gifContent = $(this).prevAll();
+                var gifButton = $(this);
+                gifContent.remove();
+                gifButton.remove();
+            });  
+        }
+    }
+    // Empties everything from #favs section
+    function clearFavs() {
         $("#fav").empty();
+    }
+
+    // GAME CLICK EVENTS
+    // Renders gifs when one of the buttons is clicked
+    $(document).on("click", ".tv-show", renderGifs);
+    // Toggles animation of gifs when the image is clicked
+    $(document).on("click", ".gif", animateGifs);
+    // Adds button according to the user's input
+    $("#add-show").on("click", addShow);
+    // Enables functionality of adding to and removing from Favorites section
+    $("#gif-area").on("click", ".add-fav", addFavorites);
+    // Empties everything from #favs section
+    $("#clear-favs").on("click", clearFavs);
+
+    // MEDIA CLICK EVENTS
+    $("#daria-play").on("click", function(){
+        daria.get(0).play();
+    });
+    $("#arnold-play").on("click", function(){
+        arnold.get(0).play();
+    });
+    $("#prince-play").on("click", function(){
+        prince.get(0).play();
+    });
+    $("#sailor-play").on("click", function(){
+        sailor.get(0).play();
+    });
+    $("#power-play").on("click", function(){
+        power.get(0).play();
+    });
+    $("#all-play").on("click", function(){
+        allThat.get(0).play();
+    });
+    // Pauses any of the buttons
+    $("#pause").on("click", function(){
+        daria.get(0).pause();
+        arnold.get(0).pause();
+        prince.get(0).pause();
+        sailor.get(0).pause();
+        power.get(0).pause();
+        allThat.get(0).pause();
     });
 
     renderButtons();
-})
+});
